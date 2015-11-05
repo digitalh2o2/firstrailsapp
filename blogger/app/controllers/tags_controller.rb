01992f5f6ffe03aff59de67a775d7c0b3283e428
Saul Ocampo <digitalh2o@localhost.localdomain>
2015-11-04 23:24:49 -0500

    Finished tagging portion!

--- blogger/app/controllers/tags_controller.rb
+++ blogger/app/controllers/tags_controller.rb
@@ -2,4 +2,17 @@ class TagsController < ApplicationController
 	def show
 		@tag = Tag.find(params[:id])
 	end
+
+	def index
+		@tag = Tag.all
+	end
+
+	def destroy
+		@tag = Tag.destroy(params[:id])
+		@tag.destroy
+		
+		flash.notice = "Tag '#{@tag.name}' Destroyed!"
+
+		redirect_to action: "index"
+	end
 end
